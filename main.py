5369af015fbf486f326319bf9e016dec49ec0ba8
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-25 21:16:54 +0530

    add headers , request module

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+import requests
+
+# main_url = "http://devilishere666.000webhostapp.com/index.html"
+url = "http://devilishere666.000webhostapp.com/mailer.php"
+
+headers = {
+	'Host': 'devilishere666.000webhostapp.com',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Language': 'en,en-US;q=0.5',
+	'Accept-Encoding': 'gzip, deflate',
+	'Content-Type': 'application/x-www-form-urlencoded'
+	'Content-Length': '121'
+	'Origin': 'http://devilishere666.000webhostapp.com'
+	'Connection': 'close'
+	'Referer': 'http://devilishere666.000webhostapp.com/index.html'
+	'Cookie': '_ga=GA1.2.1953321504.1637271960'
+	'Upgrade-Insecure-Requests': '1'
+
+}
