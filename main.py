3d98bad18bb1ae490db5ad7c2c7a8dd2fe052e2d
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-25 21:40:39 +0530

    ask for user input

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import requests
 # main_url = "http://devilishere666.000webhostapp.com/index.html"
 url = "http://devilishere666.000webhostapp.com/mailer.php"
 
+
+sender_name = input("Enter Sender Name:")
+sender_email = input("Enter Sender Email:")
+subject = input("Enter Subject:")
+reciever_email = input("Enter Reciever Email:")
+message = input("Enter Message:")
+
+#header of request analasis from burp with making request from website
 headers = {
 	'Host': 'devilishere666.000webhostapp.com',
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
@@ -19,16 +27,15 @@ headers = {
 	'Upgrade-Insecure-Requests': '1',
 }
 
-
+#data for making request
 data = {
-	's_name': 'admin',
-	's_email': 'admin@fetr.ac.in',
-	'subject': 'about test',
-	'r_email': 'padsalatrushal11@gmail.com',
-	'message':'this is test #8',
+	's_name': sender_name,
+	's_email': sender_email,
+	'subject': subject,
+	'r_email': reciever_email,
+	'message': message,
 }
 
-
 r = requests.post(url, headers=headers, data=data, allow_redirects=True)
-res = r.text
 print(r.text)
+print(r.status_code)
