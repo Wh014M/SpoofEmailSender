950886fcc0ef8b8867e3eed69f5df73eccdcead6
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-25 21:25:03 +0530

    add data for making request

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ headers = {
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Language': 'en,en-US;q=0.5',
 	'Accept-Encoding': 'gzip, deflate',
-	'Content-Type': 'application/x-www-form-urlencoded'
-	'Content-Length': '121'
-	'Origin': 'http://devilishere666.000webhostapp.com'
-	'Connection': 'close'
-	'Referer': 'http://devilishere666.000webhostapp.com/index.html'
-	'Cookie': '_ga=GA1.2.1953321504.1637271960'
-	'Upgrade-Insecure-Requests': '1'
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'Content-Length': '121',
+	'Origin': 'http://devilishere666.000webhostapp.com',
+	'Connection': 'close',
+	'Referer': 'http://devilishere666.000webhostapp.com/index.html',
+	'Cookie': '_ga=GA1.2.1953321504.1637271960',
+	'Upgrade-Insecure-Requests': '1',
+}
 
+
+data = {
+	's_name': 'admin',
+	's_email': 'admin@fetr.ac.in',
+	'subject': 'about test',
+	'r_email': 'padsalatrushal11@gmail.com',
+	'message':'this is test #8',
 }
