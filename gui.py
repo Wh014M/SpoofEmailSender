2e0dddd55826a007d31a9304e1e1afdcda80fbb6
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-26 10:09:16 +0530

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from tkinter import *
+root = Tk()
+
+root.geometry('722x400')
+root.title('Spoof Email Sender by Padsala Tushal')
+
+
+'''
+Label(root, text="Welcome to Harry Travels", font="comicsansms 13 bold", pady=15).grid(row=0, column=3)
+
+'''
+Label(root, text='Spoof Email Sender' , font='comicsansms 33 bold',pady=15, padx=225).grid(row=0, column=0)
+
+
+'''
+sender_name = input("Enter Sender Name:")
+sender_email = input("Enter Sender Email:")
+subject = input("Enter Subject:")
+reciever_email = input("Enter Reciever Email:")
+message = input("Enter Message:")
+
+#Text for our form
+name = Label(root, text="Name")
+phone = Label(root, text="Phone")
+gender = Label(root, text="Gender")
+emergency = Label(root, text="Emergency Contact")
+paymentmode = Label(root, text="Payment Mode")
+
+'''
+
+
+
+
+
+
+
+
+
+root.mainloop()
