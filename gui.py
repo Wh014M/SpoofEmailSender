3078f316ec993236f403c58cff4532df5ce20fc2
Padsala Tushal <57517785+padsalatushal@users.noreply.github.com>
2021-11-26 10:18:03 +0530

    add field in grid

--- gui.py
+++ gui.py
@@ -13,9 +13,6 @@ Label(root, text="Welcome to Harry Travels", font="comicsansms 13 bold", pady=15
 Label(root, text='Spoof Email Sender' , font='comicsansms 33 bold',pady=15, padx=225).grid(row=0, column=0)
 
 
-'''
-
-'''
 
 #Text for our form
 sender_name = Label(root, text='Sender Name')
@@ -24,9 +21,26 @@ subject = Label(root, text='Subject')
 reciever_email = Label(root, text='Reciever Email')
 message = Label(root, text='Message') 
 
+'''
 
+'''
+#Pack text for our form
+sender_name.grid(row=1 , column=2)
+sender_email.grid(row=2, column=2)
+subject.grid(row=3, column=2)
+reciever_email.grid(row=4, column=2)
+message.grid(row=5, column=2)
 
+'''
+# Tkinter variable for storing entries
+namevalue = StringVar()
+phonevalue = StringVar()
+gendervalue = StringVar()
+emergencyvalue = StringVar()
+paymentmodevalue = StringVar()
+foodservicevalue = IntVar()
 
+'''
 
 
 
